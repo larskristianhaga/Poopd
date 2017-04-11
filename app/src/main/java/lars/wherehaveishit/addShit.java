dc2eed8a79b0e466a0e90a54ef5987f9418d5f0a
LarsKristianHaga <LarsKHaga@gmail.com>
2017-04-11 23:43:30 +0200

    added more todo's

--- app/src/main/java/lars/wherehaveishit/addShit.java
+++ app/src/main/java/lars/wherehaveishit/addShit.java
@@ -5,6 +5,11 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
+import java.io.File;
+
+import static lars.wherehaveishit.R.id.ratingBar;
+import static lars.wherehaveishit.R.id.txt_ShitName;
+
 public class addShit extends AppCompatActivity {
 
     @Override
@@ -15,11 +20,26 @@ public class addShit extends AppCompatActivity {
     }
 
 
+    protected void savingFile()
+    {
+        //TODO: Save name
+        //TODO: Save rating
+        //TODO: Save cordinates
 
+        // feks:
+        // Lars Hjemme|5.5|15.35334,43.434434
+    }
 
 
     protected void doneShitting(View view)
     {
+
+        savingFile();
+
+
+
+
+        // Goes back to maps view
         Intent Maps = new Intent(this,MapsActivity.class);
         Maps.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
         startActivity(Maps);
