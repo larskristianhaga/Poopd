6d4be9949b3c7ec4141e40ae77001fa9401c1fb0
LarsKristianHaga <LarsKHaga@gmail.com>
2017-04-12 16:36:19 +0200

    working prototype now

--- app/src/main/java/lars/wherehaveishit/addShit.java
+++ app/src/main/java/lars/wherehaveishit/addShit.java
@@ -1,16 +1,27 @@
 package lars.wherehaveishit;
 
 import android.content.Intent;
+import android.location.Location;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+
+import com.google.android.gms.common.api.GoogleApiClient;
 
 import java.io.File;
 
+import static android.R.attr.tag;
+import static android.R.id.message;
 import static lars.wherehaveishit.R.id.ratingBar;
 import static lars.wherehaveishit.R.id.txt_ShitName;
 
 public class addShit extends AppCompatActivity {
+
+    // Declaring what information i need
+    protected String shitName;
+    protected String shitRating;
+    protected String lastLocation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -19,6 +30,18 @@ public class addShit extends AppCompatActivity {
         setContentView(R.layout.activity_add_shit);
     }
 
+    protected void doneShitting(View view)
+    {
+
+        savingFile();
+
+
+
+        // Goes back to maps view
+        Intent Maps = new Intent(this,MapsActivity.class);
+        Maps.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        startActivity(Maps);
+    }
 
     protected void savingFile()
     {
@@ -31,18 +54,16 @@ public class addShit extends AppCompatActivity {
     }
 
 
-    protected void doneShitting(View view)
+/*
+    protected Location getLastLocation (GoogleApiClient client)
     {
 
-        savingFile();
 
-
-
-
-        // Goes back to maps view
-        Intent Maps = new Intent(this,MapsActivity.class);
-        Maps.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-        startActivity(Maps);
+        return null;
     }
+*/
+
+
+
 
 }
