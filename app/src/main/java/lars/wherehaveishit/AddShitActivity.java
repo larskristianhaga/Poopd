dedc6977153ec02b500ec43d81783bec5e18cd22
LarsKristianHaga <LarsKHaga@gmail.com>
2017-05-25 22:09:33 +0200

    changed button color, placement and added zoom controls to the map

--- app/src/main/java/lars/wherehaveishit/AddShitActivity.java
+++ app/src/main/java/lars/wherehaveishit/AddShitActivity.java
@@ -2,27 +2,17 @@ package lars.wherehaveishit;
 
 import android.content.DialogInterface;
 import android.content.Intent;
-import android.location.Location;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 
-import com.google.android.gms.common.api.GoogleApiClient;
-
-import java.io.File;
-
-import static android.R.attr.tag;
-import static android.R.id.message;
-import static lars.wherehaveishit.R.id.ratingBar;
-import static lars.wherehaveishit.R.id.txt_ShitName;
-
-public class addShit extends AppCompatActivity {
+public class addshitActivity extends AppCompatActivity {
 
     // Declaring what information i need
     protected String shitName;
     protected String shitRating;
+    protected String shitTime;
     protected String lastLocation;
 
     @Override
@@ -40,7 +30,7 @@ public class addShit extends AppCompatActivity {
 
 
         // Goes back to maps view
-        Intent Maps = new Intent(this,MapsActivity.class);
+        Intent Maps = new Intent(this,MainActivity.class);
         Maps.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
         startActivity(Maps);
     }
--- app/src/main/java/lars/wherehaveishit/MainActivity.java
+++ app/src/main/java/lars/wherehaveishit/MainActivity.java
@@ -1,45 +1,33 @@
 package lars.wherehaveishit;
 
 import android.Manifest;
-import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
-import android.location.Criteria;
 import android.location.Location;
-import android.location.LocationManager;
-import android.os.Build;
 import android.os.Bundle;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
-import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AlertDialog;
 import android.view.View;
-import android.view.animation.Interpolator;
-import android.widget.EditText;
-import android.widget.Toast;
 
-import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
-import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.MarkerOptions;
 
-import static android.provider.AlarmClock.EXTRA_MESSAGE;
-import static lars.wherehaveishit.R.id.addShit;
 import static lars.wherehaveishit.R.id.map;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+public class MainActivity extends FragmentActivity implements OnMapReadyCallback {
     private GoogleMap mMap;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_maps);
+        setContentView(R.layout.activity_main);
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(map);
         mapFragment.getMapAsync(this);
@@ -52,6 +40,9 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
         mMap.getUiSettings().setCompassEnabled(true);
         mMap.getUiSettings().setMyLocationButtonEnabled(true);
+        mMap.getUiSettings().setZoomControlsEnabled(true);
+
+
 
 
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
@@ -78,7 +69,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     // Creates a new Intent and changes over to it
     protected void btn_addShit(View view)
     {
-        Intent addShit = new Intent(this,addShit.class);
+        Intent addShit = new Intent(this,AddShitActivity.class);
         startActivity(addShit);
 
     }
