518ae94b37d6cb82ed889c352dbdd4b9650156a9
LarsKristianHaga <LarsKHaga@gmail.com>
2017-10-05 00:52:33 +0200

    small changes

---
+++ app/src/main/java/lars/wherehaveishit/StatisticsActivity.java
@@ -0,0 +1,16 @@
+package lars.wherehaveishit;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class StatisticsActivity extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate( Bundle savedInstanceState )
+    {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_statistics);
+    }
+}
