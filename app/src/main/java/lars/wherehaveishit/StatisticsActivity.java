5986ed8d029233630f182e3bc4fa7c896ab2ad9b
LarsKristianHaga <LarsKHaga@gmail.com>
2017-10-05 22:29:23 +0200

    Added a way to give feedback to developer

--- app/src/main/java/lars/wherehaveishit/StatisticsActivity.java
+++ app/src/main/java/lars/wherehaveishit/StatisticsActivity.java
@@ -4,12 +4,13 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class StatisticsActivity extends AppCompatActivity
 {
-    //int totalNumberOfShits;
 
     @Override
     protected void onCreate( Bundle savedInstanceState )
@@ -30,5 +31,31 @@ public class StatisticsActivity extends AppCompatActivity
         Log.i("shits", String.valueOf(numberOfShitsFromMain));
         Log.i("shits", String.valueOf(totalNumberOfShits));
 
+        TextView sendFeedbackToDev = (TextView) findViewById(R.id.mailFeedbackToDeveloper);
+
+        sendFeedbackToDev.setOnClickListener(new View.OnClickListener()
+        {
+
+            @Override
+            public void onClick( View v )
+            {
+
+                Intent sendFeedback = new Intent(Intent.ACTION_SEND);
+                sendFeedback.setType("message/rfc822");
+                sendFeedback.putExtra(Intent.EXTRA_EMAIL, new String[]{"LarsKHaga@gmail.com"});
+                sendFeedback.putExtra(Intent.EXTRA_SUBJECT, "");
+                sendFeedback.putExtra(Intent.EXTRA_TEXT, "");
+                try
+                {
+                    startActivity(Intent.createChooser(sendFeedback, "Send mail to developer"));
+                } catch (android.content.ActivityNotFoundException ex)
+                {
+                    Toast.makeText(StatisticsActivity.this, "There are no email clients installed.", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+
     }
+
+
 }
