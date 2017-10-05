ae2fb184c3d22266c836f09d7e760a06dc310668
LarsKristianHaga <LarsKHaga@gmail.com>
2017-10-05 22:22:14 +0200

    added a way to sed feedback to the developer

--- app/src/main/java/lars/wherehaveishit/AboutActivity.java
+++ app/src/main/java/lars/wherehaveishit/AboutActivity.java
@@ -1,7 +1,11 @@
 package lars.wherehaveishit;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class AboutActivity extends AppCompatActivity
 {
@@ -12,5 +16,31 @@ public class AboutActivity extends AppCompatActivity
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
+
+        TextView sendFeedbackToDev = (TextView) findViewById(R.id.mailFeedbackToDeveloper);
+
+        sendFeedbackToDev.setOnClickListener(new View.OnClickListener()
+        {
+
+            @Override
+            public void onClick( View v )
+            {
+
+                Intent sendFeedback = new Intent(Intent.ACTION_SEND);
+                sendFeedback.setType("message/rfc822");
+                sendFeedback.putExtra(Intent.EXTRA_EMAIL, new String[]{"LarsKHaga@gmail.com"});
+                sendFeedback.putExtra(Intent.EXTRA_SUBJECT, "");
+                sendFeedback.putExtra(Intent.EXTRA_TEXT, "");
+                try
+                {
+                    startActivity(Intent.createChooser(sendFeedback, "Send mail to developer"));
+                } catch (android.content.ActivityNotFoundException ex)
+                {
+                    Toast.makeText(AboutActivity.this, "There are no email clients installed.", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+
+
     }
 }
--- app/src/main/java/lars/wherehaveishit/HowToUseActivity.java
+++ app/src/main/java/lars/wherehaveishit/HowToUseActivity.java
@@ -1,7 +1,12 @@
 package lars.wherehaveishit;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class HowToUseActivity extends AppCompatActivity
 {
@@ -12,5 +17,32 @@ public class HowToUseActivity extends AppCompatActivity
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_how_to_use);
+
+        TextView sendFeedbackToDev = (TextView) findViewById(R.id.mailFeedbackToDeveloper);
+
+        sendFeedbackToDev.setOnClickListener(new View.OnClickListener()
+        {
+
+            @Override
+            public void onClick( View v )
+            {
+
+                Intent sendFeedback = new Intent(Intent.ACTION_SEND);
+                sendFeedback.setType("message/rfc822");
+                sendFeedback.putExtra(Intent.EXTRA_EMAIL, new String[]{"LarsKHaga@gmail.com"});
+                sendFeedback.putExtra(Intent.EXTRA_SUBJECT, "");
+                sendFeedback.putExtra(Intent.EXTRA_TEXT, "");
+                try
+                {
+                    startActivity(Intent.createChooser(sendFeedback, "Send mail to developer"));
+                } catch (android.content.ActivityNotFoundException ex)
+                {
+                    Toast.makeText(HowToUseActivity.this, "There are no email clients installed.", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+
     }
+
+
 }
