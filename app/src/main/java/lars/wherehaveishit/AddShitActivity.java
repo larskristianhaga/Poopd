793d87b39e90a7fa58518b95b0d7b44448b1887e
Lars Kristian Haga <LarsKHaga@gmail.com>
2019-04-18 18:10:46 +0200

    Added null check and reformatted code

--- app/src/main/java/lars/wherehaveishit/AddShitActivity.java
+++ app/src/main/java/lars/wherehaveishit/AddShitActivity.java
@@ -1,5 +1,7 @@
 package lars.wherehaveishit;
 
+import java.util.Calendar;
+
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
@@ -13,76 +15,81 @@ import android.widget.EditText;
 import android.widget.RatingBar;
 import android.widget.Toast;
 
-import java.util.Calendar;
-
-public class AddShitActivity extends AppCompatActivity
-{
+public class AddShitActivity extends AppCompatActivity {
 
     EditText shitName;
+
     EditText shitNote;
+
     RatingBar shitRatingCleanness;
+
     RatingBar shitRatingPrivary;
+
     RatingBar shitRatingOverall;
+
     DBHandler db;
+
     MenuItem adjustMenuIcon;
+
     static String locationLat;
+
     static String locationLon;
+
     static String savingFromMapIfBackLat;
+
     static String savingFromMapIfBackLon;
+
     static float locationFromMapAccuracy;
+
     boolean locationIsAdjusted = false;
+
     String shitCustom = "No";
 
 
     @Override
-    protected void onCreate( Bundle savedInstanceState )
-    {
+    protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_shit);
 
-        shitName = (EditText) findViewById(R.id.edit_shitName);
-        shitRatingCleanness = (RatingBar) findViewById(R.id.ratingBarCleanness);
-        shitRatingPrivary = (RatingBar) findViewById(R.id.ratingBarPrivacy);
-        shitRatingOverall = (RatingBar) findViewById(R.id.ratingBarOverall);
-        shitNote = (EditText) findViewById(R.id.edit_shitNote);
+        shitName = findViewById(R.id.edit_shitName);
+        shitRatingCleanness = findViewById(R.id.ratingBarCleanness);
+        shitRatingPrivary = findViewById(R.id.ratingBarPrivacy);
+        shitRatingOverall = findViewById(R.id.ratingBarOverall);
+        shitNote = findViewById(R.id.edit_shitNote);
         db = new DBHandler(this);
 
-        try
-        {
+        try {
             Intent locationFromOtherActivity = getIntent();
             Bundle bundle = locationFromOtherActivity.getExtras();
 
-            locationLat = bundle.getString("LocationLatitude");
-            locationLon = bundle.getString("LocationLongitude");
-            locationFromMapAccuracy = bundle.getFloat("LocationAccuracy");
+            if (bundle != null) {
+                locationLat = bundle.getString("LocationLatitude");
+                locationLon = bundle.getString("LocationLongitude");
+                locationFromMapAccuracy = bundle.getFloat("LocationAccuracy");
+            }
 
             savingFromMapIfBackLat = locationLat;
             savingFromMapIfBackLon = locationLon;
 
-        } catch (NullPointerException e)
-        {
+        } catch (NullPointerException e) {
             Log.e("Location from bundle", "Location is null/or not reachable from bundle " + e.toString());
         }
 
-        shitRatingCleanness.setOnRatingBarChangeListener(new RatingBar.OnRatingBarChangeListener()
-        {
+        shitRatingCleanness.setOnRatingBarChangeListener(new RatingBar.OnRatingBarChangeListener() {
 
             @Override
-            public void onRatingChanged( RatingBar ratingBar, float rating, boolean fromUser )
-            {
+            public void onRatingChanged(RatingBar ratingBar, float rating, boolean fromUser) {
 
                 shitRatingOverall.setRating((shitRatingPrivary.getRating() + rating) / 2);
 
             }
         });
 
-        shitRatingPrivary.setOnRatingBarChangeListener(new RatingBar.OnRatingBarChangeListener()
-        {
+        shitRatingPrivary.setOnRatingBarChangeListener(new RatingBar.OnRatingBarChangeListener() {
 
             @Override
-            public void onRatingChanged( RatingBar ratingBar, float rating, boolean fromUser )
-            {
+            public void onRatingChanged(RatingBar ratingBar, float rating, boolean fromUser) {
 
                 shitRatingOverall.setRating((rating + shitRatingCleanness.getRating()) / 2);
 
@@ -92,12 +99,10 @@ public class AddShitActivity extends AppCompatActivity
     }
 
 
-    private boolean savingData( )
-    {
+    private boolean savingData() {
 
         // If there is not a value in name you cannot add the shit and it will prompt the user saying something went wrong when saving the data
-        if (shitName.getText().length() == 0)
-        {
+        if (shitName.getText().length() == 0) {
             Toast.makeText(this, getApplicationContext().getString(R.string.type_in_name), Toast.LENGTH_LONG).show();
             Log.i("returns", "name.getText().length() == 0");
             return false;
@@ -120,14 +125,13 @@ public class AddShitActivity extends AppCompatActivity
 
         Shit shit;
 
-        if (locationLon == null || locationLat == null)
-        {
+        if (locationLon == null || locationLat == null) {
             Log.i("LocationEmpty", "locationLon og locationLat is empty");
-            shit = new Shit(shitNameFin, shitDateFin, savingFromMapIfBackLon, savingFromMapIfBackLat, shitRatingCleannessFin, shitRatingPrivacyFin, shitRatingOverallFin, shitNoteFin, shitCustom);
-        }
-        else
-        {
-            shit = new Shit(shitNameFin, shitDateFin, locationLon, locationLat, shitRatingCleannessFin, shitRatingPrivacyFin, shitRatingOverallFin, shitNoteFin, shitCustom);
+            shit = new Shit(shitNameFin, shitDateFin, savingFromMapIfBackLon, savingFromMapIfBackLat, shitRatingCleannessFin, shitRatingPrivacyFin,
+                    shitRatingOverallFin, shitNoteFin, shitCustom);
+        } else {
+            shit = new Shit(shitNameFin, shitDateFin, locationLon, locationLat, shitRatingCleannessFin, shitRatingPrivacyFin, shitRatingOverallFin,
+                    shitNoteFin, shitCustom);
         }
 
         db.addShit(shit);
@@ -138,18 +142,15 @@ public class AddShitActivity extends AppCompatActivity
 
     // Prompt the users and ask if the don't want to save their shit
     @Override
-    public void onBackPressed( )
-    {
+    public void onBackPressed() {
 
         new AlertDialog.Builder(this)
                 .setIcon(android.R.drawable.ic_dialog_alert)
                 .setMessage(getApplicationContext().getString(R.string.sure_you_want_to_disregard))
-                .setPositiveButton(getApplicationContext().getString(R.string.yes), new DialogInterface.OnClickListener()
-                {
+                .setPositiveButton(getApplicationContext().getString(R.string.yes), new DialogInterface.OnClickListener() {
 
                     @Override
-                    public void onClick( DialogInterface dialog, int which )
-                    {
+                    public void onClick(DialogInterface dialog, int which) {
 
                         finish();
                     }
@@ -160,8 +161,7 @@ public class AddShitActivity extends AppCompatActivity
     }
 
     @Override
-    public boolean onCreateOptionsMenu( Menu menu )
-    {
+    public boolean onCreateOptionsMenu(Menu menu) {
 
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.add_poop_menu, menu);
@@ -171,15 +171,12 @@ public class AddShitActivity extends AppCompatActivity
     }
 
     @Override
-    public boolean onOptionsItemSelected( MenuItem item )
-    {
+    public boolean onOptionsItemSelected(MenuItem item) {
 
-        switch (item.getItemId())
-        {
+        switch (item.getItemId()) {
             case R.id.editLocation:
                 // Handles the menu press
                 Log.i("Menu", "editLocation pressed");
-
 
                 Intent adjustLocation = new Intent(AddShitActivity.this, EditLocationActivity.class);
                 adjustLocation.putExtra("LocationLatitude", locationLat);
@@ -189,13 +186,10 @@ public class AddShitActivity extends AppCompatActivity
                 startActivityForResult(adjustLocation, 555);
                 break;
             case R.id.done:
-                if (savingData())
-                {
+                if (savingData()) {
                     finish();
 
-                }
-                else
-                {
+                } else {
                     // Displays a text saying that data was not saved properly, if dataSaved != true.
                     Toast.makeText(AddShitActivity.this, getApplicationContext().getString(R.string.not_saved_properly), Toast.LENGTH_LONG).show();
                 }
@@ -207,14 +201,11 @@ public class AddShitActivity extends AppCompatActivity
     }
 
     @Override
-    protected void onActivityResult( int requestCode, int resultCode, Intent data )
-    {
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
 
         super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == 555)
-        {
-            if (resultCode == RESULT_OK)
-            {
+        if (requestCode == 555) {
+            if (resultCode == RESULT_OK) {
                 locationLat = data.getStringExtra("LocationLatitude");
                 locationLon = data.getStringExtra("LocationLongitude");
 
@@ -222,8 +213,7 @@ public class AddShitActivity extends AppCompatActivity
 
                 locationIsAdjusted = true;
             }
-            if (resultCode == RESULT_CANCELED)
-            {
+            if (resultCode == RESULT_CANCELED) {
                 Log.i("resultCode", "RESULT_CANCELED");
                 locationIsAdjusted = false;
             }
